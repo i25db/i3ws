4cda662d2111d1448f168902c001ac22873aae74
i25db <i25.db@outlook.com>
2021-12-26 04:28:48 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use clap::{App, AppSettings, Arg, ArgSettings, ArgMatches};
+
+fn main() {
+    let matches = get_matches();
+}
+
+fn get_matches() -> ArgMatches {
+    App::new("i3ws")
+        .author("i25db <i25.db@outlook.com>")
+        .version("v0.0.1")
+        .about("A CLI tool for managing i3 workspaces")
+        .setting(AppSettings::SubcommandRequired)
+        .setting(AppSettings::DisableHelpSubcommand)
+        .subcommand(
+            App::new("go")
+                .short_flag('g')
+                .about("Go to a workspace")
+                .arg(
+                    Arg::new("create")
+                        .short('c')
+                        .long("create")
+                        .help("Create new sub workspace if one doesn't exist")
+                )
+                .arg(
+                    Arg::new("workspace")
+                        .takes_value(true)
+                        .possible_values(["main", "sub"])
+                        .setting(ArgSettings::Required)
+                )
+                .setting(AppSettings::ArgRequiredElseHelp)
+        )
+        .subcommand(
+            App::new("new")
+                .short_flag('n')
+                .about("Creates a new preset workspace")
+                .arg(
+                    Arg::new("new")
+                        .takes_value(true)
+                        .possible_values(&["plain", "code", "game"])
+                        .setting(ArgSettings::Required)
+                )
+        )
+        .get_matches()
+}
